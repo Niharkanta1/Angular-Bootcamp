7041b399da4be1d8f3ae1d549a07e48e040581e8
Nihar Kanta Tripathy <nitr0722@netcracker.com>
2022-10-06 18:22:04 +0530

    wiki-search app

---
+++ wiki-search/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { WikipediaService } from './service/wikipedia.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pages: any[] = [];
+  constructor(private wikiservice: WikipediaService) {
+
+  }
+  
+  onSearch(event : any) {
+    this.wikiservice.search(event).subscribe((response: any) => {
+      this.pages = response.query.search;
+    })
+  }
+}
